3f9ceb93b048d4ffbea64381ca2ad45b7ba7cac8
itzsrikanth <snow.650@gmail.com>
2018-08-04 22:51:30 +0530

    added products; changes in index;

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,23 @@ var router = express.Router();
 // var fs = require('fs');
 var menu = require('../data/menu.json');
 var products = require('../data/products.json');
+var comments = require('../data/comments.json');
+var parallelogram = require('../data/parallelogram.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     menu: menu,
-    products: products
+    products: products,
+    comments: comments
+  });
+});
+
+router.get('/products', function(req, res) {
+  res.render('products', {
+    menu: menu,
+    products: products,
+    parallelogram: parallelogram
   });
 });
 
