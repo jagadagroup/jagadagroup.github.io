34e5f3c8682b576ace50e3f768bef25bf92cdc75
itzsrikanth <snow.650@gmail.com>
2018-09-15 23:52:54 +0530

    menu changes; overall touches

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,32 @@
 $('#ham-burger').click(e => console.log);
 console.log($('#ham-burger'));
+
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() + 200 > $('.em-circle').offset().top) {
+        // trigger
+        $(window).off('scroll');
+        var data = [];
+        console.log(this);
+
+        // $('.em-circle').each(function(i, div) {
+            $('.em-circle > span').each(function(i, span) {
+                data.push(+span.innerText);
+                span.innerText = 0;
+            });
+            var count = 0;
+            var timer = setInterval(function() {
+                $('.em-circle > span').each(function(i, span) {
+                    if (data[i] > +span.innerText) {
+                        span.innerText = +span.innerText + 1;
+                    }
+                });
+                if (count > 10) {
+                    clearInterval(timer);
+                } else {
+                    count++;
+                }
+            }, 100);
+        // });
+    }
+})
